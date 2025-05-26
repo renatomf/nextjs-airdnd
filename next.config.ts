5b64a42e8efd90360fbbfbeabe90675038501d8c
renatomarf76 <renatomarf76@egmail.com>
2025-05-26 16:45:42 -0300

    Creating Filter Bar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        hostname: "a0.muscache.com",
+        protocol: "https",
+        port: "",
+      },
+    ],
+  },
+};  
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { MapFilterItems } from "@/components/MapFilterItems";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="container mx-auto px-5 lg:px-10">
+      <MapFilterItems />
     </div>
 
   );
